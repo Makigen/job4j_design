ff2e382593f88f006fc7871561b0098bebe66996
Makigen <makige.ee@mail.ru>
2021-04-19 19:36:32 +0300

    Checkstyle

--- src/main/java/ru/job4j/App.java
+++ src/main/java/ru/job4j/App.java
@@ -4,7 +4,7 @@ package ru.job4j;
  * Hello world!
  *
  */
-@SuppressWarnings("checkstyle:LeftCurly")
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
