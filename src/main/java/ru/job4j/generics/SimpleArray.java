1a61be835556ff86c50254852d06ca8827544ba2
Makigen <makige.ee@mail.ru>
2021-04-23 10:54:31 +0300

    5.2.1. Реализовать SimpleArray<T> [#522269]

--- src/main/java/ru/job4j/generics/SimpleArray.java
+++ src/main/java/ru/job4j/generics/SimpleArray.java
@@ -43,7 +43,7 @@ public class SimpleArray<T> implements Iterable<T> {
 
             @Override
             public boolean hasNext() {
-                return point < elements.length;
+                return point < index;
             }
 
             @Override
