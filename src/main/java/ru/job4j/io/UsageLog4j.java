cfa896e9c0ee4bf2fb7e73a3342b866be568c610
Makigen <makige.ee@mail.ru>
2021-05-23 00:09:24 +0300

    2. Simple Loggin Facade 4 Java. [#268849]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,10 +8,15 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        char ch = 'K';
+        int i = 33;
+        long l = 123456789090L;
+        byte b = 127;
+        short s = 9876;
+        double d = 0.7;
+        float f = 987.654F;
+        boolean bl = true;
+
+        LOG.debug("Variables char : {}, int : {}, long : {}, byte : {}, short : {}, double : {}, float : {}, boolean : {}, ", ch, i, l, b, s, d, f, bl);
     }
 }
