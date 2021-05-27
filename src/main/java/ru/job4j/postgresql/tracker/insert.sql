4fab1d112fd0cd3dff4dda546cda2377e40ee605
Makigen <makige.ee@mail.ru>
2021-05-27 23:40:13 +0300

    1. Схема прав пользователи и роли. [#1093]

---
+++ src/main/java/ru/job4j/postgresql/tracker/insert.sql
@@ -0,0 +1,10 @@
+insert into users(name, role_id) values('Kirill Malikov', 1);
+insert into role(name) values('Student');
+insert into rules(rule) values('SOME_TEXT');
+insert into item(name, users_id, category_id, state_id) values('item1', 1, 2, 3);
+insert into comments(text, item_id) values('comment', 1);
+insert into attachs(name, item_id) values('comment', 1);
+insert into state(name) values('completed');
+insert into category(name) values('urgent');
+insert into role_rules(role_id, rules_id) values(1, 1);
+
