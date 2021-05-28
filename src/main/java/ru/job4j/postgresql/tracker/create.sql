c995a91983a3ed7b17f9b62a90a8aa08fe8ac7e7
Makigen <makige.ee@mail.ru>
2021-05-28 22:29:27 +0300

    1. Схема прав пользователи и роли. [#1093]

--- src/main/java/ru/job4j/postgresql/tracker/create.sql
+++ src/main/java/ru/job4j/postgresql/tracker/create.sql
@@ -1,17 +1,33 @@
+create table role (
+    id serial primary key,
+    name varchar(255)
+);
+
 create table users (
     id serial primary key,
     name varchar(255)
     role_id int references role(id)
 );
 
-create table role (
+create table rules (
+    id serial primary key,
+    rule text
+);
+
+create table state (
     id serial primary key,
     name varchar(255)
 );
 
-create table rules (
+create table category (
     id serial primary key,
-    rule text
+    name varchar(255)
+);
+
+create table role_rules (
+    id serial primary key,
+    role_id int references role(id)
+    rules_id int references rules(id)
 );
 
 create table item (
@@ -34,18 +50,5 @@ create table attachs (
     item_id int references item(id)
 );
 
-create table state (
-    id serial primary key,
-    name varchar(255)
-);
 
-create table category (
-    id serial primary key,
-    name varchar(255)
-);
 
-create table role_rules (
-    id serial primary key,
-    role_id int references role(id)
-    rules_id int references rules(id)
-);
