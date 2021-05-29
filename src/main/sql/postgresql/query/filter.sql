08a05303a0e93f20787ab4344eab35183becec03
Makigen <makige.ee@mail.ru>
2021-05-30 00:29:28 +0300

    Фильтры [#1732]

--- src/main/sql/postgresql/query/filter.sql
+++ src/main/sql/postgresql/query/filter.sql
@@ -5,3 +5,4 @@ select * from product where price = (select max(price) from product);
 select t.name, count(p.type_id) from product as p right outer join type as t on p.type_id = t.id group by t.name;
 select * from product as p join type t on t.id = p.type_id and (t.name = 'СЫР' or t.name = 'МОЛОКО');
 select t.name from product as p right outer join type as t on p.type_id = t.id group by t.name having count(p.type_id) < 10;
+select * from product as p join type t on t.id = p.type_id;
