83d6817c3e315badfb90febfa4e9b7fbd67a7c61
Makigen <makige.ee@mail.ru>
2021-06-16 23:32:20 +0300

    Демонстрация работы GC [#1589]

---
+++ src/main/java/ru/job4j/gc/User.java
@@ -0,0 +1,21 @@
+package ru.job4j.gc;
+
+public class User {
+
+    private int id;
+    private String name;
+
+    public User() {
+
+    }
+
+    public User(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        System.out.printf("Removed %d %s%n", id, name);
+    }
+}
